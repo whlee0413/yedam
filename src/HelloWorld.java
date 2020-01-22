00ffdf305e399a6a39b14d0f41dffda9d4601b2b
User <User@YD02-14>
2020-01-22 11:09:55 +0900

    0122

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int age = 30;
+		int year = 2020;
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("���ش� " + year);
+		System.out.println( "�� ���̴� " + age + " �Դϴ�.");
+		
+	
+	}
+}
+
---
+++ src/VariableClass.java
@@ -0,0 +1,13 @@
+
+public class VariableClass {
+	public static void main(String[] args) {
+		int num1,num2,num3;
+		 num1 = 100;
+		 num2 = 90;
+		 num3 = 80; 
+		
+		int sum = num1 + num2 +num3;
+		System.out.println("sum의 값" + sum);
+	}
+
+}
